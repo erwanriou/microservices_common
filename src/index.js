2bdb2054228239deedcd75e7588ae50a302f2b70
Erwan <rr.1.riou@gmail.com>
2020-10-14 18:36:51 -0500

    [Library] - update version

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 "use strict"
 
-console.log("test");
+module.exports = factory = (file) => {
+  return require(`./factory/${file}`)
+}
+
+module.exports = middlewares = (file) => {
+  return require(`./middlewares/${file}`)
+}
