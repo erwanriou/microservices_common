14cadd41c85e9c386885729cf99737a4734a80b0
Erwan <rr.1.riou@gmail.com>
2020-10-14 17:52:15 -0500

    [Library] - update version

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,8 +1,8 @@
-echo ":: UPDATE NPM VERSION"
-npm version patch
 echo ":: COMMIT AND DEPLOY CHANGES"
 git add .
 git commit -m '[Library] - update version'
 git push
+echo ":: UPDATE NPM VERSION"
+npm version patch
 echo ":: PUBLISH TO NPM"
 npm publish
