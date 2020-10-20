fa8c4bec78ef41857e15b9ec7ea1b4e2029496ec
Erwan <rr.1.riou@gmail.com>
2020-10-19 20:05:17 -0500

    [Library] - update version

---
+++ src/factory/types.js
@@ -0,0 +1,12 @@
+// DEFINE ALL LISTENER SUBJECTS
+const subject = {
+  TICKET_CREATED: "ticket:created"
+}
+
+// DEFINE ALL LISTENER QUEUE GROUP NAMES
+const queueGroupName = {
+  PAYMENT_SERVICE: "payment-service"
+}
+
+exports.subject = subject
+exports.queueGroupName = queueGroupName
