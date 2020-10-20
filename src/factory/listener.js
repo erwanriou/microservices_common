525938bb157629f91e4ddc66dcbb985876cd6770
Erwan <rr.1.riou@gmail.com>
2020-10-19 20:24:10 -0500

    [Library] - update version

--- src/factory/listener.js
+++ src/factory/listener.js
@@ -39,16 +39,4 @@ class Listener {
   }
 }
 
-// DEFINE ALL LISTENER SUBJECTS
-const Subject = {
-  TICKET_CREATED: "ticket:created"
-}
-
-// DEFINE ALL LISTENER QUEUE GROUP NAMES
-const QueueGroupName = {
-  PAYMENT_SERVICE: "payment-service"
-}
-
-exports.Subject = Subject
-exports.QueueGroupName = QueueGroupName
 exports.Listener = Listener
---
+++ src/factory/types.js
@@ -0,0 +1,12 @@
+// DEFINE ALL LISTENER SUBJECTS
+const Subject = {
+  TICKET_CREATED: "ticket:created"
+}
+
+// DEFINE ALL LISTENER QUEUE GROUP NAMES
+const QueueGroupName = {
+  PAYMENT_SERVICE: "payment-service"
+}
+
+exports.Subject = Subject
+exports.QueueGroupName = QueueGroupName
