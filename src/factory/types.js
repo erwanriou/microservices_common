be55ace866128eda6100d6a0203aba1fe692bced
Erwan <rr.1.riou@gmail.com>
2020-10-30 14:09:18 -0600

    [Library] - update version

--- src/factory/types.js
+++ src/factory/types.js
@@ -12,7 +12,8 @@ const QueueGroupName = {
   PAYMENT_SERVICE: "payment-service",
   ORDER_SERVICE: "order-service",
   TICKET_SERVICE: "ticket-service",
-  EXPIRATION_SERVICE: "expiration-service"
+  EXPIRATION_SERVICE: "expiration-service",
+  PAYMENT_SERVICE: "payment-service"
 }
 
 exports.Subject = Subject
